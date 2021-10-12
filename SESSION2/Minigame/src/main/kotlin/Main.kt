bc09ff3a02aced5dcf447e30df8014da3ff6d34f
JasonRoossens <jason.roossens@student.ehb.be>
2021-10-12 12:03:03 +0200

    Start challenge 1

--- SESSION2/Minigame/src/main/kotlin/Main.kt
+++ SESSION2/Minigame/src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 fun main() {
-    fun challengeOne(){
+    println("Hello")
+    println("Choose the sport i like")
 
-    }
+}
+
+fun challengeOne(){
+    val sports = setOf("football", "rugby", "golf","baseball","basketball")
+
+    if {
+        //win
+    }else
+        //lose
 }
