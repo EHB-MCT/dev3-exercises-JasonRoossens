0581838fb1864d57d040cdd42e0fff3a6a2949e0
JasonRoossens <jason.roossens@student.ehb.be>
2021-10-13 16:26:28 +0200

    Challenge one done

--- SESSION2/AdventureGame/src/main/kotlin/Main.kt
+++ SESSION2/AdventureGame/src/main/kotlin/Main.kt
@@ -1,2 +1,16 @@
-class Main {
+fun main (){
+    println("It's january 1st 2022, the Zombie apocalypse has just begun. Will you be able to survive?")
+    challengeOne()
 }
+
+fun challengeOne() {
+    println("How many times per week did you work out on average in 2021?")
+    var numberOfWorkouts = readLine()?.toInt()
+    val notEnoughWorkouts = setOf(0,1)
+    if (notEnoughWorkouts.contains(numberOfWorkouts)){
+        println("sorry, you're dead")
+    } else  {
+        println("congrats you might survive")
+    }
+}
+
