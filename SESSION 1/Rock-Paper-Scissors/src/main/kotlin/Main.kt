0bafb9dad80c13b688a8b85504eceb8d9e012efb
JasonRoossens <jason.roossens@student.ehb.be>
2021-10-13 16:08:46 +0200

    Updates rock paper scissors project

--- SESSION 1/Rock-Paper-Scissors/src/main/kotlin/Main.kt
+++ SESSION 1/Rock-Paper-Scissors/src/main/kotlin/Main.kt
@@ -1,23 +1,37 @@
 fun main(){
-    println("Make your choice:")
-    var userchoice = userChooses()
+    val options: Array<String> = arrayOf("Rock", "Paper", "Scissors")
+    val computerChoice = options.random()
+    val userChoice = getUserChoice(options)
 
-    println("The computer chose:")
-    var options = arrayOf("rock", "paper", "scissors")
-    var computerchoice = options[(0..2).random()]
-    println(computerchoice)
-
-    message(userchoice, computerchoice)
+    println("User picked \"$userChoice\"")
+    println("Computer picked \"$computerChoice\"")
+    determineWinner(computerChoice, userChoice)
 }
 
-fun userChooses(userchoice:String?){
-    val userchoice = readLine()
-    println("You have entered this: $userchoice")
-    return userchoice:String?
+fun getUserChoice(options: Array<String>): String {
+    var userChoiceObtained:String = ""
+    do {
+        println("Chose between Rock / Paper / Scissors")
+        userChoiceObtained = readLine().toString()
+    }while (!options.contains(userChoiceObtained))
+    return  userChoiceObtained
 }
 
-fun message (x:String,y:String){
-    if (userchoice == computerchoice){
-        println("it's a tie!")
-        return
-    }}
+fun determineWinner(computerChoice: String, userChoice: String){
+
+    // Tied game
+    if(computerChoice == userChoice){
+        print("You tied!")
+    }
+
+    // Win or lose
+    when(computerChoice){
+        "Rock" -> if (userChoice == "Paper"){
+            println("You won!")
+        }else if (userChoice == "Scissors"){
+            println("You lost!")
+        }
+        "Paper" -> if (userChoice == "Rock") println("You lost!") else if (userChoice == "Scissors") println("You won!")
+        "Scissors" -> if (userChoice == "Paper") println("You lost!") else if (userChoice == "Rock") println("You won!")
+    }
+}
