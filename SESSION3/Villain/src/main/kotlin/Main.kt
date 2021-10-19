e798b146d179860ff6c8f879db5e1963ff501ae2
JasonRoossens <jason.roossens@student.ehb.be>
2021-10-19 10:20:13 +0200

    Problem name fixed

--- SESSION3/Villain/src/main/kotlin/Main.kt
+++ SESSION3/Villain/src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ class Main {
     fun main () {
         val dries = Hero("Dries")
         val jens = Hero("Jens")
-        val Matthias = Villain(name: "Matthias")
+        val matthias = Villain("Matthias")
     }
     dries.talk()
     jens.talk()
