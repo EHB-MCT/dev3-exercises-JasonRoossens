2c600c212c205a89b60333a1e4a78aeedf1741f3
JasonRoossens <jason.roossens@student.ehb.be>
2021-10-19 10:18:35 +0200

    Villain project WIP

--- SESSION3/Villain/src/main/kotlin/Main.kt
+++ SESSION3/Villain/src/main/kotlin/Main.kt
@@ -1,2 +1,11 @@
 class Main {
+    fun main () {
+        val dries = Hero("Dries")
+        val jens = Hero("Jens")
+        val Matthias = Villain(name: "Matthias")
+    }
+    dries.talk()
+    jens.talk()
+    matthias.talk()
+
 }
