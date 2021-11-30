96775d1a7127814e5056edc5a49cb263ca2ee750
JasonRoossens <jason.roossens@student.ehb.be>
2021-11-30 10:04:25 +0100

    Start session7

--- SESSION6/src/main/kotlin/main.kt
+++ SESSION6/src/main/kotlin/main.kt
@@ -16,10 +16,14 @@ fun main() {
                 credentials.databaseName +
                 ":" + "3306" + "/" + credentials.databaseUser, connectionProps)
 
-    val statement = connection.prepareStatement("SELECT * FROM products")
+    val statement = connection.prepareStatement("SELECT * FROM trains WHERE name = ?")
 // The result of your query will be a type ResultSet
 // This is a bit similar to collections in Kotlin
 // But since the library is based on Java, we get a type from Java
+    statement.setString(1, search)
     val result = statement.executeQuery()
+    while(resultSet.next()) {
+        result.getString("column_name")
+    }
     println(result.toString())
 }
